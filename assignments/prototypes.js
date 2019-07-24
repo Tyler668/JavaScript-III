2baa899ccc79b350ef84e1dfc0b67079c2b999cf
Tyler Gilchrist <tyler668@gmail.com>
2019-07-24 18:58:52 -0400

    Made the stretch game

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -26,8 +26,6 @@ function GameObject(char) {
     this.dimensions = char.dimensions
 };
 
-
-
 /*
   === CharacterStats ===
   * healthPoints
@@ -43,19 +41,8 @@ CharacterStats.prototype.takeDamage = function () {
 
 function CharacterStats(char) {
   this.healthPoints = char.healthPoints,
-  GameObject.call(this, char);
+    GameObject.call(this, char);
 };
-
-
-
-
-
-// const newPlayer = new GameObject(tyler);
-// console.log(newPlayer);
-// console.log(newPlayer.destroy())
-
-
-// console.log(newChar.destroy());
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -67,19 +54,44 @@ function CharacterStats(char) {
   * should inherit takeDamage() from CharacterStats
 */
 
+
 Humanoid.prototype = Object.create(CharacterStats.prototype);
 
-Humanoid.prototype.greet = function(){
-return `${this.name} offers a greeting in ${this.language}!`;
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}!`;
 };
 
-function Humanoid(char){
+Humanoid.prototype.attack = function (target) {
+  const atkDamage = damageRoll(5);
+  target.healthPoints = target.healthPoints - atkDamage;
+  return `${this.name} attacked ${target.name}, dealing ${atkDamage} damage!`;
+};
+
+
+//Calculates random damage for the attack 1-5 ----------|
+function damageRoll(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+//------------------------------------------------------|
+
+function Humanoid(char) {
   this.team = char.team,
-  this.weapons = char.weapons,
-  this.language = char.language,
-  GameObject.call(this, char);
+    this.weapons = char.weapons,
+    this.language = char.language,
+    CharacterStats.call(this, char);
 };
 
+Hero.prototype = Object.create(Humanoid.prototype);
+
+function Hero(char) {
+  Humanoid.call(this, char);
+};
+
+
+Villain.prototype = Object.create(Humanoid.prototype);
+function Villain(char) {
+  Humanoid.call(this, char);
+};
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -89,68 +101,124 @@ function Humanoid(char){
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
 
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+const wizard = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 20,
+  name: 'Merlin',
+  team: 'Mystic Tribe',
+  weapons: [
+    'Staff',
+    'Book',
+  ],
+  language: 'Deep Speak',
+});
+
+const barbarian = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 20,
+  name: 'Grognak',
+  team: 'Plains Tribe',
+  weapons: [
+    'Hammer',
+    'Axe',
+  ],
+  language: 'Common',
+});
 
 
- 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+//The fight:
+while (wizard.healthPoints > 0 && barbarian.healthPoints > 0) {
+
+  console.log(wizard.attack(barbarian));
+  console.log("Grognak HP - " + barbarian.healthPoints + "    Merlin HP - " + wizard.healthPoints);
+
+  console.log(barbarian.attack(wizard));
+  console.log("Grognak HP - " + barbarian.healthPoints + "    Merlin HP - " + wizard.healthPoints);
+
+}
+
+if (wizard.healthPoints <= 0) {
+  console.log(`${wizard.name} died!`);
+}
+
+if (barbarian.healthPoints <= 0) {
+  console.log(`${barbarian.name} died!`);
+}
+
+
 
 
   // Stretch task: 
